080f0410c2c7a6c2558215e3b1d2696269b236c8
Leenxch <62754797+Leenxch@users.noreply.github.com>
2020-04-05 10:28:34 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+// var randomNumber1er = Math.random();
+// randomNumber1= randomNumber1*6;
+// randomNumber1=Math.floor(randomNumber1)+1;
+
+var randomNumber1= Math.floor(Math.random()*6)+1;
+document.querySelector(".img1").setAttribute("src","images/dice"+randomNumber1+".png");
+var randomNumber2= Math.floor(Math.random()*6)+1;
+document.querySelector(".img2").setAttribute("src","images/dice"+randomNumber2+".png");
+if (randomNumber1 > randomNumber2){
+  document.querySelector("h1").innerHTML = (" 🚩Player 1 Wins");
+
+}else if (randomNumber2 > randomNumber1){
+  document.querySelector("h1").innerHTML = ("Player 2 Wins! 🚩");
+} else {
+  document.querySelector("h1").innerHTML = ("Draw!");
+}
